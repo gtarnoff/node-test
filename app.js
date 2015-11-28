7450473122d975fd1ef79101543bbda01b2bf978
Greg Tarnoff <greg.tarnoff@gmail.com>
2015-11-28 12:13:33 -0600

    routing test.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function(req, res) { //req == request from browser, res = =response from server
   res.send('Hello World!');
+});
+
+app.post('/', function(req, res) {
+  res.send('Got a post');
+});
+
+app.put('/user', function (req, res) {
+  res.send('got a PUT request at /user');
+});
+
+app.delete('/user', function(req, res){
+  res.send('Got a delete request at /user');
 });
 
 var server = app.listen(3000, function(){
