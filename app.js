4001cb61a681af5b64f7d6c8b91710347b126909
Greg Tarnoff <greg.tarnoff@gmail.com>
2015-11-28 11:04:20 -0600

    Node Express Sample App
    First commit for a node express sample application based on expressjs.com

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) { //req == request from browser, res = =response from server
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('example app listening at http://%s:%s', host, port);
+});
